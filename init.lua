0f96e5a781cd9bd9536c044480fb2c67709c7d6b
Meth <adcockliam6@gmail.com>
2025-08-21 04:27:24 +0100

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,6 @@
+-- Scanner Init
+include("megascanner/config.lua")
+include("megascanner/errorhud.lua")
+include("megascanner/conflictscanner.lua")
+include("megascanner/cleanup.lua")
+include("megascanner/onboarding.lua")
